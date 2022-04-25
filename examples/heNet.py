873454ca035ca8ab4fc9a61619538cbba4a5b0b1
Sebb S <sebbi-k@t-online.de>
2022-04-25 23:15:39 +0200

    Added small library and test file

---
+++ examples/heNet.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_prime(x):
+    return 1-np.tanh(x)**2
+
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x))
+
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+def square(x):
+    return x * x
+
+def square_prime(x):
+    return 2 * x
+
+def approx_sigmoid(x):
+    return 0.5 + 0.197 * x - 0.0004 * (x**3)
+
+def approx_sigmoid_prime(x):
+    return approx_sigmoid(x)*(1 - approx_sigmoid(x))
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / y_true.size
+
+def cross_entropy(y_true, y_pred):
+    return np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+def cross_entropy_prime(y_true, y_pred):
+    return y_pred - y_true
+
+class BaseLayer():
+
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    def forward(self, input):
+        pass
+
+    def backward(self, output_error, learning_rate):
+        pass
+
+class FullyConnectedLayer(BaseLayer):
+
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.rand(input_size, output_size) * 0.5
+        self.bias = np.random.rand(1, output_size) * 0.5
+
+    def forward(self, input_data):
+        self.input = input_data
+        self.output = np.dot(self.input, self.weights) + self.bias
+        return self.output
+
+    def backward(self, output_error, learning_rate):
+        input_error = np.dot(output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error)
+
+        self.weights -= learning_rate * weights_error
+        self.bias -= learning_rate * output_error
+        return input_error
+
+class ActivationLayer(BaseLayer):
+
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input_data):
+        self.input = input_data
+        self.output = self.activation(self.input)
+        return self.output
+
+    def backward(self, output_error, learning_rate):
+        return self.activation_prime(self.input) * output_error
+
+class Network:
+    def __init__(self, debug):
+        self.layers = []
+        self.loss = None
+        self.loss_prime = None
+        self.debug = debug
+
+    def add(self, layer):
+        self.layers.append(layer)
+
+    def use(self, loss, loss_prime):
+        self.loss = loss
+        self.loss_prime = loss_prime
+
+    def predict(self, input_data):
+        samples = len(input_data)
+        result = []
+
+        for i in range(samples):
+            output = input_data[i]
+            for layer in self.layers:
+                output = layer.forward(output)
+            result.append(output)
+
+        return result
+
+    def fit(self, x_train, y_train, epochs, learning_rate):
+        samples = len(x_train)
+
+        for i in range(epochs):
+            err = 0
+            for j in range(samples):
+                output = x_train[j]
+                for layer in self.layers:
+                    output = layer.forward(output)
+
+                err += self.loss(y_train[j], output)
+
+                error = self.loss_prime(y_train[j], output)
+                for layer in reversed(self.layers):
+                    error = layer.backward(error, learning_rate)
+
+            err /= samples
+            if self.debug and i % 50 == 0:
+                print('epoch %d/%d   error=%f' % (i+1, epochs, err))
